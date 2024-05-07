242cb2fb80197c37ccfa93dac4e0c27babb74738
Gerardo Santana <gs.amezcua@comunidad.unam.mx>
2024-05-07 16:57:27 -0600

    Realizada la primera parte

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const imgPoke1 = document.querySelector('#poke1');
+const namePoke1 = document.querySelector('#nombrePoke-propio');
+const poke1Tipo = document.querySelector('#tipoPropio');
+const poke1Ataque = document.querySelector('#ataquePropio');
+
 const imgPoke2 = document.querySelector('#poke2');
 const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
+
+const input = document.querySelector('#input');
+const btn_poke = document.querySelector('#btn-poke');
+const btn_ataque = document.querySelector('#combate');
 
 
 const getNumRandom = () => {
@@ -12,7 +21,17 @@ const getNumRandom = () => {
   }
 
 const obtenerPokePropio = ()=>{
+    const numPokePropio = input.value;
 
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokePropio}`).then((res)=> {
+        return res.data;
+    }).then((res)=>{
+        console.log(res);
+        imgPoke1.src = res.sprites.back_default;
+        poke1Tipo.innerHTML = res.types[0].type.name;
+        namePoke1.innerHTML = res.name;
+        poke1Ataque.innerHTML = res.stats[0].base_stat;
+    })
 }
 
 const obtenerPokeRival = () =>{
@@ -32,10 +51,10 @@ const obtenerPokeRival = () =>{
 }
 
 const combate = ()=>{
-    
+
 }
 
 window.addEventListener('load', obtenerPokeRival)
-
-
+btn_poke.addEventListener('click', obtenerPokePropio)
+btn_ataque.addEventListener('click', combate)
 
